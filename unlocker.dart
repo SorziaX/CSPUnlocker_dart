c48a3d86bae00e219881190bfde22e5d1ee765b6
zhouxuan <zhouxuan.aschar@bytedance.com>
2021-04-29 14:08:03 +0800

    增加配置文件能力

--- unlocker.dart
+++ unlocker.dart
@@ -1,81 +1,86 @@
 import 'dart:io';
+import 'package:intl/intl.dart';
 
 void main() {
   test();
 }
-const SRC_FILE_NAME = "csp";
-const SRC_FILE_EXTENSION = "txt";
+
 const NEW_FILE_EXTENSION = "new";
 const BAK_FILE_EXTENSION = "bak";
 const CONFIG_FILE_PATH = "csp_unlocker.conf ";
 
+String fileName = "";
+String fileExt= "";
+List<int> targetBytes = new List<int>();
+List<int> replaceBytes = new List<int>();
+int bytesLength = 0;
+int replaceCount = 0;
+
 Future<void> test() async {
-  File srcFile = new File("$SRC_FILE_NAME.$SRC_FILE_EXTENSION");
-  File newFile = new File("$SRC_FILE_NAME.$NEW_FILE_EXTENSION");
-  File config = new File(CONFIG_FILE_PATH);
-  List<String> list = await config.readAsLines();
+  //加载配置文件
+  await loadConfig();
+
+  //打开文件
+  File srcFile = new File("$fileName.$fileExt");
+  File newFile = new File("$fileName.$NEW_FILE_EXTENSION");
   RandomAccessFile srcFileAccess = await srcFile.open();
   RandomAccessFile newFileAccess = await newFile.open(mode: FileMode.write);
 
-  int length = (list[0].length/2).round();
 
-  List<int> buffer = new List<int>();
-  List<int> target = new List<int>();
-  List<int> replace = new List<int>();
+  //创建buffer
+  List<int> bufferBytes = new List<int>();
   int byte = 0;
 
-  for(int i = 0; i < list[0].length;i+=2){
-    String s2 = list[0].substring(i,i+2);
-    int i2 = int.parse(s2,radix: 16);
-    target.add(i2);
-    print(i2.toRadixString(16));
-  }print("");
-  for(int i = 0; i < list[1].length;i+=2){
-    String s2 = list[1].substring(i,i+2);
-    int i2 = int.parse(s2,radix: 16);
-    replace.add(i2);
-    print(i2.toRadixString(16));
-  }print("");
-
-  for(int i = 0;i < length;i++){
+  //预读一定长度的buffer，方便对比
+  for(int i = 0;i < bytesLength;i++){
     byte = await srcFileAccess.readByteSync();
-    buffer.add(byte);
+    bufferBytes.add(byte);
   }
 
+
+  //循环对比
   do{
-    if(compareList(buffer,target)){
-      for(int i = 0;i < length;i++){
+    if(replaceCount == 0 && compareList(bufferBytes,targetBytes)){
+      //替换字节串
+      replaceCount++;
+      print("$bufferBytes  => $replaceBytes");
+      for(int i = 0;i < bytesLength;i++){
         byte = await srcFileAccess.readByteSync();
-        await newFileAccess.writeByte(replace[i]);
-        print(replace[i].toRadixString(16));
-        buffer.removeAt(0);
-        buffer.add(byte);
+        await newFileAccess.writeByte(replaceBytes[i]);
+        bufferBytes.removeAt(0);
+        bufferBytes.add(byte);
       }
       continue;
     }else{
-      await newFileAccess.writeByte(buffer[0]);
+      await newFileAccess.writeByte(bufferBytes[0]);
     }
 
     byte = await srcFileAccess.readByteSync();
     if(byte == -1)
       break;
 
-    buffer.removeAt(0);
-    buffer.add(byte);
+    bufferBytes.removeAt(0);
+    bufferBytes.add(byte);
     
   }while(true);
 
-  for(int i = 1;i < length;i++){
-    await newFileAccess.writeByte(buffer[i]);
+  //文件已读到底，替换buffer内剩余串
+  for(int i = 1;i < bytesLength;i++){
+    await newFileAccess.writeByte(bufferBytes[i]);
   }
 
+  //关闭文件
   await srcFileAccess.close();
   await newFileAccess.close();
 
-  await srcFile.rename("$SRC_FILE_NAME.$BAK_FILE_EXTENSION");
-  await newFile.rename("$SRC_FILE_NAME.$SRC_FILE_EXTENSION");
+  //修改文件名
+  //String bakFileExt = await availableBakExt(fileName);
+  String bakFileExt = await timeBakExt();
+  await srcFile.rename("$fileName.$bakFileExt");
+  await newFile.rename("$fileName.$fileExt");
 }
 
+//对比字节串
 bool compareList(List<int> list1,List<int> list2){
   for(int i = 0; i < list1.length ; i++){
     if(list1[i] != list2[i]){
@@ -83,4 +88,49 @@ bool compareList(List<int> list1,List<int> list2){
     }
   }
   return true;
+}
+
+//读取配置文件
+void loadConfig() async{
+  File config = new File(CONFIG_FILE_PATH);
+
+  List<String> list = await config.readAsLines();
+  String targetFileName = list[0];
+  int dotIndex = targetFileName.lastIndexOf('.');
+  fileName = targetFileName.substring(0,dotIndex);
+  fileExt = targetFileName.substring(dotIndex+1,targetFileName.length);
+
+  for(int i = 0; i < list[1].length;i+=2){
+    String s2 = list[1].substring(i,i+2);
+    int i2 = int.parse(s2,radix: 16);
+    targetBytes.add(i2);
+  }
+  for(int i = 0; i < list[2].length;i+=2){
+    String s2 = list[2].substring(i,i+2);
+    int i2 = int.parse(s2,radix: 16);
+    replaceBytes.add(i2);
+  }
+
+  bytesLength = (list[1].length/2).round();
+}
+
+//获取可用扩展名
+Future<String> availableBakExt(String fileName) async{
+  File file;
+  String ext;
+  int i = 0;
+  do{
+    ext = "$BAK_FILE_EXTENSION${i == 0 ? '':i}";
+    file = new File("$fileName.$ext");
+    i++;
+  }while(await file.exists());
+
+  return ext;
+}
+
+//根据时间生成扩展名
+String timeBakExt(){
+  var now = DateTime.now();
+  var formatter = new DateFormat("yyyy-MM-dd HH-mm-ss");
+  return "${BAK_FILE_EXTENSION}_${formatter.format(now)}";
 }
